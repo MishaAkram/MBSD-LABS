fb8cf57fb48b28561a2c4d5704a48f69c61bb7cc
MishaAkram <mishaakrambaig@gmail.com>
2021-08-10 19:50:11 +0500

    lab 10 added

---
+++ lab10/lab10Reciever/lab10Reciever/main.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#define F_CPU 1000000UL
+#include<util/delay.h>
+int main(void)
+{
+	DDRD&=~(1<<PD0);
+	DDRD|=(1<<PD1);
+	DDRA=0xFF;
+	DDRC=0xFF;
+	//int UBRR_Value=25;
+	//UBRR0H=(unsigned char)(UBRR_Value>>8);
+	//UBRR0L=(unsigned char)UBRR_Value;
+	UBRR0H=0x00;
+	UBRR0L=0x05;
+	UCSR0B=(1<<RXEN0);
+	UCSR0C=(1<<USBS0)|(3<<UCSZ00);
+unsigned char receiveData;
+while(1)
+{ PORTC=0;
+	receiveData=0;
+	while(!(UCSR0A&(1<<RXC0)));
+	receiveData=UDR0;
+	PORTC=receiveData;
+	if(receiveData==0b11110000)
+	{
+		PORTA^=(1<<PA0);
+		_delay_ms(1000);
+	}
+}
+}
---
+++ lab10/lab10Transmitter/lab10Transmitter/main.c
@@ -0,0 +1,26 @@
+#include <avr/io.h>
+#define F_CPU 1000000UL
+#include<util/delay.h>
+int main(void)
+{
+	DDRD|=1<<PD1;
+	DDRD&=~(1<<PD0);
+	PORTD|=1<<PD0;
+	DDRA=0x00;
+	//int UBBRValue=25;
+	UBRR0H=0x00;
+	UBRR0L=0x05;
+	UCSR0B=(1<<TXEN0);
+	UCSR0C=(1<<USBS0)|(3<<UCSZ00);
+	char y=0;
+	while(1)
+	{
+		y=PINA&0b00000001;
+		if(y==0)
+		{
+			while(!(UCSR0A&(1<<UDRE0)));
+			UDR0=0b11110000;
+			_delay_ms(1000);
+		}
+	}
+}
